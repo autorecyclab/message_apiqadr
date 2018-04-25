a4524733dfc176fa24664e15c0340d4c66efad73
Ludovic PIGEON <ludovic.pigeon@laposte.fr>
2018-04-25 11:12:52 +0200

    nex_repository

---
+++ message_apiqadr.js
@@ -0,0 +1,17 @@
+welcome_page_messages = [{
+
+        "date": "25/12",
+
+        "message": "Joyeux noël, nous arrêterons le service temporairement pour traiter toutes les adresses du père noël."
+
+                },
+
+    {
+
+        "date": "01/01",
+
+        "message": "Un arret de service pour maintenance aura lieu de 8 h00 à 9 h00."
+
+                }
+
+]
