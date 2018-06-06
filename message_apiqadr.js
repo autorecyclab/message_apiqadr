45f80c6d89cfde6290b7156a3afa5816b591cb45
LUDO33500 <ludovic.pigeon@laposte.fr>
2018-06-06 14:13:06 +0200

    test sur ttes les ihm

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,1 +1,16 @@
-var welcome_page_messages = [];
+var welcome_page_messages = [{
+
+                               "date": "25/12",
+
+                               "message": "Joyeux noël, nous arrêterons le service temporairement pour traiter toutes les adresses du père noël."
+
+                },
+
+                {
+
+                               "date": "01/01",
+
+                               "message": "Un arret de service pour maintenance aura lieu de 8 h00 à 9 h00."
+
+                }
+                            ];
