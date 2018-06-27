b2b4ddc619fb7d5c4b410a0b66ac2e110a67c200
LUDO33500 <ludovic.pigeon@laposte.fr>
2018-06-27 11:21:12 +0200

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,6 +1,6 @@
 var welcome_page_messages = 
 [
-  { "date": "28/06/2018",
-    "message": "MEP APIQADR à partir de 9h00, le service risque d'être perturbé durant la matinée."
+  { "date": "27/06/2018",
+    "message": "Mise en production de la version 2_10 de l'API qualification des adresses le 28/06/2018 à partir de 09h00. Le service risque d'être perturbé durant la matinée."
   }
 ]
