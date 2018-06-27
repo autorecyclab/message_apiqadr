596cd7c9a1bbeb6e1845ce6ebb17597fb691931e
LUDO33500 <ludovic.pigeon@laposte.fr>
2018-06-27 10:54:04 +0200

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,1 +1,6 @@
-var welcome_page_messages = [];
+var welcome_page_messages = 
+[
+  { "date": "28/06/2018",
+    "message": "MEP APIQADR à partir de 9h00, le service risque d'être perturbé durant la matinée."
+  }
+]
