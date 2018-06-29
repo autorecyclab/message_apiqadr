01c831f8fc04a13ca56f5c052a19e3c8ef34e978
Claude Seguret <clodio@users.noreply.github.com>
2018-06-29 10:10:52 +0200

    Suppression du message

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,6 +1,1 @@
-var welcome_page_messages = 
-[
-  { "date": "27/06/2018",
-    "message": "Mise en production de la version 2_10 de l'API qualification des adresses le 28/06/2018 à partir de 09h00. Le service risque d'être perturbé durant la matinée."
-  }
-]
+var welcome_page_messages = []
