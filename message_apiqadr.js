fd2663fa66aa385daf9b3610ad113702d869df4c
Claude Seguret <clodio@users.noreply.github.com>
2018-09-17 16:50:24 +0200

    Ajout de message suite à une opération vtom

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,1 +1,6 @@
-var welcome_page_messages = []
+var welcome_page_messages = 
+[
+  { "date": "17/09/2018",
+    "message": "Des opérations d'infrastructures informatiques vont avoir lieu le 18/09 et 20/09 de 10h00 à 12h00. Les traitements de fichiers peuvent être potentiellement plus longs que d'habitude. Désolé pour la gêne occasoionnée"
+  }
+]
