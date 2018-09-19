c11dbd600ee9cdf17eb13cfce476c70a4b9474f2
Claude Seguret <clodio@users.noreply.github.com>
2018-09-19 16:53:34 +0200

    Update message_apiqadr.js

--- message_apiqadr.js
+++ message_apiqadr.js
@@ -1,6 +1,6 @@
 var welcome_page_messages = 
 [
   { "date": "17/09/2018",
-    "message": "Des opérations d'infrastructures informatiques vont avoir lieu le 18/09 et 20/09 de 10h00 à 12h00. Les traitements de fichiers peuvent être potentiellement plus longs que d'habitude. Désolé pour la gêne occasoionnée"
+    "message": "Des opérations d'infrastructures informatiques vont avoir lieu le 20/09 de 14h00 à 17h00. Les traitements de fichiers peuvent être potentiellement plus longs que d'habitude. Désolé pour la gêne occasoionnée"
   }
 ]
